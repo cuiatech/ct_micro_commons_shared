66c925f0284182a53c528ed14b0562ee8a5c73ba
Raphael Pontes <raphael.pontes@condoconta.com.br>
2023-01-30 22:00:36 -0300

    Removing Code Coupling

--- lib/flut_micro_commons_shared.dart
+++ lib/flut_micro_commons_shared.dart
@@ -2,3 +2,5 @@ library flut_micro_commons_shared;
 
 export 'shared/components/components.dart';
 export 'shared/utils/utils.dart';
+export 'shared/utils/env.dart';
+export 'shared/components/local_storage/local_storage.dart';
---
+++ lib/shared/components/local_storage/local_storage.dart
@@ -0,0 +1,13 @@
+import 'package:flut_micro_commons_dependencies/flut_micro_commons_dependencies.dart';
+
+class LocalStorage {
+  static Future<String?> get(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(key);
+  }
+
+  static Future<bool> set(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    return await prefs.setString(key, value);
+  }
+}
